81674eaafa638f91385def175731cc007f3e26ac
tatianesantoro <santoro.tatiane@gmail.com>
2025-10-29 21:23:28 +0800

    adicionando trabalho final

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    reportPageTitle: 'Relatório de Testes Cypress',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    code: false,
+    timestamp: 'dd-mm-yyyy_HH-MM-ss'
+  },
   e2e: {
     baseUrl: 'https://automationexercise.com',
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
     viewportWidth: 1280,
     viewportHeight: 720,
